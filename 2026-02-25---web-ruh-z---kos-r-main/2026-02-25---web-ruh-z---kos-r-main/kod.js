6cc0e6d7427d630f3a464a85e18a27f631c955b0
Pápai Roland <144841960+papairoland@users.noreply.github.com>
2026-02-25 13:48:20 +0100

    nagyitKep visszaKep kesz

---
+++ 2026-02-25---web-ruh-z---kos-r-main/2026-02-25---web-ruh-z---kos-r-main/kod.js
@@ -0,0 +1,11 @@
+function nagyitKep(melyikKep) {
+    let nagyito = document.getElementById("nagykep");
+    nagyito.src = melyikKep;
+}
+
+function visszaKep() {
+    document.getElementById("nagykep").src = "ures.jpg";
+}
+
+function kivalaszt(melyikTermek) {
+}
