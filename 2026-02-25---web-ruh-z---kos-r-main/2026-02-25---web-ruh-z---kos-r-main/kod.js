e25cdb1be97ab1b7342b02dd8b7ff1fd947e956b
Pápai Roland <144841960+papairoland@users.noreply.github.com>
2026-02-27 10:12:56 +0100

    szamol function

--- 2026-02-25---web-ruh-z---kos-r-main/2026-02-25---web-ruh-z---kos-r-main/kod.js
+++ 2026-02-25---web-ruh-z---kos-r-main/2026-02-25---web-ruh-z---kos-r-main/kod.js
@@ -7,13 +7,27 @@ function visszaKep() {
     document.getElementById("nagykep").src = "ures.jpg";
 }
 
-function kivalaszt(melyikTermek) {
-    let termek = document.getElementById(melyikTermek);
-    let darab = window.prompt("Hány darabot szeretne?");
+function szamol(){
+    let ram_darab = document.getElementById("db1").value;
+    let ram_reszosszeg = ram_darab * 25000;
+    document.getElementById("resz1").innerHTML = ram_reszosszeg + " Ft";
 
-    if (darab != null && darab != "") {
-        let kosar = document.getElementById("kosar");
-        let nev = termek.innerHTML;
-        kosar.innerHTML += "<p>" + darab + " db " + nev + "</p>";
-    }
+    let monitor_darab = document.getElementById("db2").value;
+    let monitor_reszosszeg = monitor_darab * 55000;
+    document.getElementById("resz2").innerHTML = monitor_reszosszeg + " Ft";
+
+    let hdd_darab = document.getElementById("db3").value;
+    let hdd_reszosszeg = hdd_darab * 30000;
+    document.getElementById("resz3").innerHTML = hdd_reszosszeg + " Ft";
+
+    let ssd_darab = document.getElementById("db4").value;
+    let ssd_reszosszeg = ssd_darab * 42000;
+    document.getElementById("resz4").innerHTML = ssd_reszosszeg + " Ft";
+
+    let tap_darab = document.getElementById("db5").value;
+    let tap_reszosszeg = tap_darab * 15000;
+    document.getElementById("resz5").innerHTML = tap_reszosszeg + " Ft";
+
+    let vegosszeg = ram_reszosszeg + monitor_reszosszeg + hdd_reszosszeg + ssd_reszosszeg + tap_reszosszeg;
+    document.getElementById("osszertek").innerHTML = vegosszeg + " Ft";
 }
